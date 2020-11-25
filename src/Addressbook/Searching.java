2309a562d915a9dc9a4a4f072049680c15a1809f
manikiranp <manikiranp3@gmail.com>
2020-11-25 08:47:16 +0530

    displays number of persons count by city or state

--- src/Addressbook/Searching.java
+++ src/Addressbook/Searching.java
@@ -28,28 +28,35 @@ public class Searching {
 	public void searchcity() {
 		System.out.println("Enter City to search");
 		name=inp.nextLine();
+		int count=0;
 		for (Person p:AddressbookManager.contacts) {
 			if(p.city.toLowerCase().equals(name.toLowerCase())) {
 				found=true;
+				count++;
 				System.out.println(p.toString());
 			}
+			
 		}
+		System.out.println("\nNo. of contact persons by city:"+name+" "+count);
 			if(found==false) {
-				System.out.println("Not found");
+				System.out.println("\nNot found");
 			}
 		
 	}
 	public void searchstate() {
 		System.out.println("Enter State to search");
 		name=inp.nextLine();
+		int count=0;
 		for (Person p:AddressbookManager.contacts) {
 			if(p.state.toLowerCase().equals(name.toLowerCase())) {
-				found=true;
+				found=true; 
+				count++;
 				System.out.println(p.toString());
 			}
 		}
+		System.out.println("\nNo. of contact persons by State:"+name+" "+count);
 			if(found==false) {
-				System.out.println("Not found");
+				System.out.println("\nNot found");
 			}
 	}
 	
